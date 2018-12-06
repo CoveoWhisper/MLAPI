9ec1ddeb6168aacd83e42dbb6a402ccb48c17f21
xahor <erdnacram15@gmail.com>
2018-12-05 21:34:19 -0500

    simplified a little

--- mlapi/document_filter.py
+++ mlapi/document_filter.py
@@ -14,7 +14,7 @@ class DocumentFilter(object):
     def keep_documents_without_facets(documents, must_not_have_facets):
         documents_with_facets = {}
         for document, facets in documents.items():
-            if not all([facet.value not in must_not_have_facets[facet.name] for facet in facets if facet.name in must_not_have_facets]):
+            if any([facet.value in must_not_have_facets[facet.name] for facet in facets if facet.name in must_not_have_facets]):
                 continue
             documents_with_facets[document] = facets
 
