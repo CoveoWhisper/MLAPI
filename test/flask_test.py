dc2c98666add7608a81cf871de861694ea083b6a
xahor <erdnacram15@gmail.com>
2018-12-05 22:54:58 -0500

    fix other test

--- test/flask_test.py
+++ test/flask_test.py
@@ -15,7 +15,7 @@ class FlaskTest(unittest.TestCase):
         self.assertEqual(result.status_code, 200)
 
     def test_filter_facets_endpoint(self):
-        request = {'Documents': ['uri1', 'uri2', 'uri3'], 'MustHaveFacets': [{'Name': 'name1', 'Value': 'value1'}, {'Name': 'name2', 'Value': 'value2'}], 'MustNotHaveFacets': [{'Name': 'name3', 'Value': 'value3'}, {'Name': 'name4', 'Value': 'value4'}]}
+        request = {'Documents': ['uri1', 'uri2', 'uri3'], 'MustHaveFacets': [{'Name': 'name1', 'Values': ['value1']}, {'Name': 'name2', 'Values': ['value2']}], 'MustNotHaveFacets': [{'Name': 'name3', 'Values': ['value3']}, {'Name': 'name4', 'Values': ['value4']}]}
         result = self.app.post('/ML/Filter/Facets', data=json.dumps(request), content_type='application/json')
         self.assertEqual(result.status_code, 200)
 
